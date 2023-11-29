8ef732f36397cde4594912ca52e2c316efaaa117
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-29 19:34:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#стек - загальне поняття через список
+stack = []
+stack.append("a")
+stack.append("b")
+stack.append("c")
+stack.append("d")
+print(stack[-1])#peek()
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack)
