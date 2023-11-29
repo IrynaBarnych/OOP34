3e2425219a60afbfd546dabb2a99eefd6e2478ea
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-29 20:03:45 +0200

    #за допомогою однозв'язного списку

--- main.py
+++ main.py
@@ -11,3 +11,79 @@ print(stack.pop())
 print(stack.pop())
 print(stack.pop())
 print(stack)
+
+from collections import deque
+
+#порожній стек
+stack = deque()
+
+stack.append("a")
+stack.append("b")
+stack.append("c")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+
+from queue import LifoQueue
+
+stack = LifoQueue(maxsize=3)
+print(stack.qsize())#кількість елементів
+stack.put("a")
+stack.put("b")
+stack.put("c")
+
+print(stack.full())
+print(stack.qsize())#кількість елементів
+for i in range(stack.qsize()):
+    print(stack.get())
+
+#за допомогою однозв'язного списку
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class Stack:
+    def __init__(self):
+        self.head = Node("head")
+        self.size = 0
+    def __str__(self):
+        cur = self.head.next
+        out = ""
+        while cur:
+            out += str(cur.value) + " -> "
+            cur = cur.next
+        return out[:-4]
+
+    def getSize(self):
+        return self.size
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("стек порожній, неможливо продивитися")
+        return self.head.next.value
+
+    def push(self, value):
+        node = Node(value)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("стек порожній, неможливо видалити")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+stack = Stack()
+for i in range(1, 11):
+    stack.push(i)
+print(stack)
+print(stack.pop())
+print(stack.peek())
+print(stack)
